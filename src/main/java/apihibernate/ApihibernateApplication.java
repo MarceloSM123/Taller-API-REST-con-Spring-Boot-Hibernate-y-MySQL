889dfe59268b8fe6b32785fab66dfb43880e08a5
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-26 18:37:42 -0500

    LMSM: Pruebas de postman exitosas

--- src/main/java/apihibernate/ApihibernateApplication.java
+++ src/main/java/apihibernate/ApihibernateApplication.java
@@ -2,12 +2,20 @@ package apihibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.krakdev", "apihibernate"})
+@EnableJpaRepositories(basePackages = "com.krakdev")
+@EntityScan(basePackages = "com.krakdev")
 public class ApihibernateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApihibernateApplication.class, args);
+		System.out.println("=== API Películas con Hibernate iniciada ===");
 	}
 
 }
